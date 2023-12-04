ecd886d6fc4f6f4661c40f995c5dc9af3a4826c1
marromagu <mromagu2611@g.educaand.es>
2023-12-04 14:12:42 +0100

    Terminado

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,12 +3,25 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,10 +43,30 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(
-    modifier: Modifier = Modifier
-        .fillMaxSize()//fillMaxSize utiliza toda la pantalla
-        .wrapContentSize(Alignment.Center)//para centrar en el centro xd
-) {//Modifier es un objeto que decora la IU
-
-
+    modifier: Modifier = Modifier//Modifier es un objeto que decora la IU
+        .fillMaxSize()// fillMaxSize utiliza toda la pantalla
+        .wrapContentSize(Alignment.Center)// Para centrar en el centro xdd
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var result by remember { mutableStateOf(1) }// La funcion mutable muestra un elemento observable
+        //var result = 1, ahora cambiamos esta variable para q sea un elemento remember
+        val imageResource = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> {
+                R.drawable.dice_6
+            }
+        }
+        Image(painter = painterResource(id = imageResource), contentDescription = "1")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {// Esto es una lambda
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
